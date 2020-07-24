4ec74bd7dd2162dd191cde0fe6d85cd5908bcdee
Ruchi <ruchipanda7@gmail.com>
2020-07-24 15:04:44 +0530

    added a file

---
+++ final/index.js
@@ -0,0 +1,16 @@
+const express= require("express");
+const app=express();
+const port=process.env.PORT || 3000
+
+app.set('view engine','ejs');
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+
+app.listen(port,()=>{
+    console.log("kol");
+});
